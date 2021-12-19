d9dd44833fa4393d98b97b15b02a51b59f5aee7c
PDA2304 <66939206+PDA2304@users.noreply.github.com>
2021-12-19 21:12:46 +0300

    Была создана миграция, модель и котроллер для таблицы Type_Action

---
+++ app/app/Http/Controllers/TypeActionController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TypeAction;
+use Illuminate\Http\Request;
+
+class TypeActionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TypeAction  $typeAction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TypeAction $typeAction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TypeAction  $typeAction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TypeAction $typeAction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TypeAction  $typeAction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TypeAction $typeAction)
+    {
+        //
+    }
+}
