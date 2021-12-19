5abb37f0ba085847b1ba663239a823c5fdf2159c
PDA2304 <66939206+PDA2304@users.noreply.github.com>
2021-12-19 21:33:18 +0300

    Реализована модель для таблицы Type_Action

--- app/app/Models/TypeAction.php
+++ app/app/Models/TypeAction.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class TypeAction extends Model
 {
     use HasFactory;
+
+    protected $table = 'type_actions';
+
+    protected $fillable = [
+        'id',
+        'action_name',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+    
 }
