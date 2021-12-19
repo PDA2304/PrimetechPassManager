cff2a52cf6e720e62a510a20774bf1ece19f2404
PDA2304 <66939206+PDA2304@users.noreply.github.com>
2021-12-19 23:43:12 +0300

    Реализация модели для таблицы Action

--- app/app/Models/Action.php
+++ app/app/Models/Action.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Action extends Model
 {
     use HasFactory;
+
+    
+    protected $table = 'actions';
+
+    protected $fillable = [
+        'id',
+        'action_date',
+        'user_id',
+        'type_action_id',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
 }
