e6e2a246addaf375a15ae700954a18d79d68782a
PDA2304 <66939206+PDA2304@users.noreply.github.com>
2021-12-20 08:45:40 +0300

    Была создана миграция, модель и контроллер для таблицы Data_User

---
+++ app/database/migrations/2021_12_20_054244_create_data_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDataUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('data_id');
+            $table->integer('user_id');
+            $table->timestamps();
+
+            $table->foreign('data_id')->references('id')->on('data')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('user_id')->references('id')->on('employees')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data_users');
+    }
+}
