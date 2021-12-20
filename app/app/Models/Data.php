cd152aad1d8654413783ff64934d0a14b0b4f634
PDA2304 <66939206+PDA2304@users.noreply.github.com>
2021-12-20 10:21:23 +0300

    Была реализована модель для таболицы Data

--- app/app/Models/Data.php
+++ app/app/Models/Data.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Data extends Model
 {
     use HasFactory;
+
+    protected $table = 'data';
+
+    protected $fillable = [
+        'id',
+        'login',
+        'password',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
 }
