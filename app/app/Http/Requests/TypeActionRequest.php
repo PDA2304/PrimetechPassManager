a9b0cd43f8711d29b3c53cbc826a715de6889533
PDA2304 <isip_d.a.pahomov@mpt.ru>
2021-12-26 18:28:00 +0300

    Была изменена валидация, чтобы было возможно вводить только быквы и пробелы в поле action_name

--- app/app/Http/Requests/TypeActionRequest.php
+++ app/app/Http/Requests/TypeActionRequest.php
@@ -9,7 +9,7 @@ class TypeActionRequest extends ApiRequest
     public function rules()
     {
         return [
-            'action_name' => ["required", "max:30", 'alpha', function ($attribute, $value, $fail) {
+            'action_name' => ["required", "max:30", 'alpha_spaces', function ($attribute, $value, $fail) {
                 if (TypeAction::where('action_name', '=', $value)->first()) {
                     $fail('Такой название уже есть');
                 }
