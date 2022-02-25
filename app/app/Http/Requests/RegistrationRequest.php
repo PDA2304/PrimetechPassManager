ff6aee0aaa6115ed1901ab84472911be60717b70
pda2304 <isip_d.a.pahomov@mpt.ru>
2022-02-25 19:05:24 +0000

    Изменена валидация данных при регистрации пользователя

--- app/app/Http/Requests/RegistrationRequest.php
+++ app/app/Http/Requests/RegistrationRequest.php
@@ -10,26 +10,22 @@ class RegistrationRequest extends ApiRequest
     public function rules()
     {
         return [
-            'user_name' => ["required", "min:3","max:30",function ($attribute, $value, $fail) {
-                if (Employee::where('user_name', '=', $value)->first()) {
-                    $fail('Taкое имя уже используется другим пользователем');
-                }
-            }],
-            'login' => ["required","email",function ($attribute, $value, $fail) {
+            'user_name' => ["required", "min:3", "max:30"],
+            'login' => ["required", "email:rfc,dns", function ($attribute, $value, $fail) {
                 if (Employee::where('login', '=', $value)->first()) {
-                    $fail('Taкой логин уже используется другим пользователем');
+                    $fail('Taкая почта есть в системе');
                 }
             }],
-            'password' => ["required",'min:8', "max:16"]
+            'password' => ["required", 'min:8', "max:16"]
         ];
     }
 
     public function attributes()
     {
         return [
-            "user_name"=>'имя пользователя',
-            "login"=>'логин',
-            "password"=>'пароль',
+            "user_name" => 'имя пользователя',
+            "login" => 'логин',
+            "password" => 'пароль',
         ];
     }
 }
