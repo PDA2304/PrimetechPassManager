68676f75b7ef06ca03ed367c6ad94d8106dd616a
Пахомов Даниил <pda@pozhavt.ru>
2022-04-02 20:58:58 +0300

    Изменение baseUrl

--- lib/data/network_service.dart
+++ lib/data/network_service.dart
@@ -9,7 +9,8 @@ import 'package:passmanager/data/model/Trash.dart';
 /// [baseUrl] Ссылка к API
 class NetworkService {
   final dio = Dio();
-  String baseUrl = "http://192.168.0.92:8888/api/";
+  String baseUrl = "http://52.250.99.102:8888/api/";
+  // String baseUrl = "http://192.168.0.92:8888/api/"; //locale
 
   /// Функция регистрации
   /// если возращается 200 регистрация прошла успешно
